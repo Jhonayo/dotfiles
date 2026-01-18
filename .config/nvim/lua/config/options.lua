04059408fe0c0ba528eb6b5c984f82d672e7adfa
jhonayo <j.iramirezpavez@gmail.com>
2026-01-18 18:00:47 -0300

    Config completa: LSP, blink.cmp, fzf-lua, neo-tree, etc

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+-- Transparencia para ventanas flotantes
+vim.opt.winblend = 15  -- 0 = opaco, 100 = totalmente transparente
+vim.opt.pumblend = 15  -- Para menús de autocompletado
+
+
+vim.opt.swapfile = false
