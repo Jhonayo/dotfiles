cc26bd0f3ec3cbcb419166ffbc8d395e057d12eb
jhonayo <j.iramirezpavez@gmail.com>
2026-02-15 22:48:05 -0300

    feat: implement template handling and include base templates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- Cargar configuración de templates
+require("config.templates")
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Terminal mode exit to normal mode
-vim.keymap.set("t", "<C-\\><C-n>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+-- ============================================================
+-- KEYMAPS: Templates
+-- ============================================================
+
+local templates = require("utils.templates")
+
+vim.keymap.set("n", "<leader>ft", function()
+  templates.select_and_insert()
+end, { desc = "Insert Template" })
+
+vim.keymap.set("n", "<leader>fg", function()
+  templates.create_gitignore()
+end, { desc = "Create .gitignore" })
